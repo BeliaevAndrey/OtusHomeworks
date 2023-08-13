2bc29cf08dd1e844f3c3099a868f05856411c0c8
Andrei Beliaev <abbot190414@gmail.com>
2023-08-13 10:50:35 +0300

    OTUS homework 003: 1. Tests results added;

--- HomeWork003/src/testPowering.java
+++ HomeWork003/src/testPowering.java
@@ -1,14 +1,16 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class testPowering {
 
     static final String directory = "HomeWork003/testData/3.Power";
+    static final String outDirectory = "HomeWork003/testsResults/3.Power.csv";
+
 
     public static void main(String[] args) {
         testPowering tp = new testPowering();
@@ -17,33 +19,28 @@ public class testPowering {
     }
 
     void test(ArrayList<double[]> tests) {
+        ArrayList<HashMap<String, double[]>> testsResults = new ArrayList<>();
+
         Powering pwr = new Powering();
         String template = "%25.12f %12.0f %20.12f || " +
-                "%18.11f %10.3e %8.3e | " +
                 "%18.11f %10.3e %8.3e | " +
                 "%18.11f %10.3e %8.3e | " +
                 "%18.11f %10.3e %8.3e |\n";
         System.out.printf("%25s %12s %20s || " +
                         "%18s %10s %9s | " +
                         "%18s %10s %9s | " +
-                        "%18s %10s %9s | " +
                         "%18s %10s %9s |\n",
                 "Number", "Power", "Answer",
-                "Iterative", "time", "delta",
                 "Binary recursive", "time", "delta",
-                "Binary iter.", "time", "delta",
-                "Binary iter. mlt.", "time", "delta");
+                "Binary iter. mlt.", "time", "delta",
+                "Iterative", "time", "delta"
+        );
         for (double[] test : tests) {
 
             double start0 = System.nanoTime();
-            double result0 = (long)(pwr.getDegreeB(test[0], (long) test[1]) * 1e11) / 1e11;
+            double result0 = (long) (pwr.getDegreeB(test[0], (long) test[1]) * 1e11) / 1e11;
             double end0 = (System.nanoTime() - start0) / 1e9;
             double delta0 = Math.abs(result0 - test[2]);
-
-            double start1 = System.nanoTime();
-            double result1 = (long)(pwr.getDegreeBinIter(test[0], (long) test[1]) * 1e11) / 1e11;
-            double end1 = (System.nanoTime() - start1) / 1e9;
-            double delta1 = Math.abs(result1 - test[2]);
 
             double start2 = System.nanoTime();
             double result2 = pwr.powerIterMult(test[0], (long) test[1]);
@@ -58,10 +55,20 @@ public class testPowering {
             System.out.printf(template,
                     test[0], test[1], test[2],
                     result0, end0, delta0,
-                    result1, end1, delta1,
                     result2, end2, delta2,
                     result3, end3, delta3);
+
+            testsResults.add(new HashMap<>() {{
+                                 put("testData", test);
+                                 put("Binary recursive", new double[]{result0, end0, delta0});
+                                 put("Binary iter. mlt.", new double[]{result2, end2, delta2});
+                                 put("Iterative", new double[]{result3, end3, delta3});
+                             }}
+            );
+
         }
+
+        writeResultsToFile(testsResults);
     }
 
     ArrayList<double[]> loadTests(String path) {
@@ -105,4 +112,36 @@ public class testPowering {
             return null;
         }
     }
+
+    void writeResultsToFile(ArrayList<HashMap<String, double[]>> testsResults) {
+        String headLine = "Number;Power;Answer;" +
+                "Binary recursive;time;delta;" +
+                "Binary iter. mlt.;time;delta;" +
+                "Iterative;time;delta;\n";
+        try (BufferedWriter bw = Files.newBufferedWriter(Path.of(outDirectory), StandardOpenOption.APPEND)) {
+            bw.write(headLine);
+            StringBuilder sb = new StringBuilder();
+            for (HashMap<String, double[]> results : testsResults) {
+                for (double rst : results.get("testData")) {
+                    sb.append(rst).append(";");
+                }
+                for (double rst : results.get("Binary recursive")) {
+                    sb.append(rst).append(";");
+                }
+                for (double rst : results.get("Binary iter. mlt.")) {
+                    sb.append(rst).append(";");
+                }
+                for (double rst : results.get("Iterative")) {
+                    sb.append(rst).append(";");
+                }
+                sb.append("\n");
+            }
+            bw.write(sb.toString());
+            bw.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
