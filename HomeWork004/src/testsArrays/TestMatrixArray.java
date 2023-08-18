53e3f8e202154cb0c5318a6321eed8dcdaf2a99b
Andrei Beliaev <abbot190414@gmail.com>
2023-08-18 15:56:21 +0300

    OTUS homework 004: test for MatrixArray refactored;

--- HomeWork004/src/testsArrays/TestMatrixArray.java
+++ HomeWork004/src/testsArrays/TestMatrixArray.java
@@ -13,64 +13,48 @@ public class TestMatrixArray {
     }
 
     void testSingleArray() {
-        IArray matrixArray = new MatrixArray();
-        testAddArray(matrixArray, 38);
-
-//        System.out.println("\nadd testing");
-//        testAddPosArray(vectorArray, 1000, "test 1");
-//        testAddPosArray(vectorArray, 1000, "test 2");
-//        testAddPosArray(vectorArray, vectorArray.size(), "test tail");
-//        testAddPosArray(vectorArray, 1000, "test 3");
-//        testAddPosArray(vectorArray, 0, "test head");
-//        testAddPosArray(vectorArray, 1001, "test 4");
-//        System.out.println("\nadd checking");
-//        for (int i = 999; i < 1010; i++) {
-//            System.out.println(i + " " + vectorArray.get(i));
-//        }
-//        System.out.println(vectorArray.get(0));
-//        System.out.println(vectorArray.get(vectorArray.size() - 1));
+        IArray<String> matrixArray = new MatrixArray<>();
+        testAddArray(matrixArray, 100_000);
 
         System.out.println("\nremove testing");
-//        testDelPosArray(vectorArray, 0);
-        testDelPosArray(matrixArray, 10);
+        testDelPosArray(matrixArray, 49999);
+        testDelPosArray(matrixArray, 0);
         testDelPosArray(matrixArray, matrixArray.size() - 1);
-        testDelPosArray(matrixArray, 10);
-        testDelPosArray(matrixArray, 10);
-        testDelPosArray(matrixArray, 10);
+        testDelPosArray(matrixArray, 50000);
+        testDelPosArray(matrixArray, 50000);
+        testDelPosArray(matrixArray, 50000);
         System.out.println("\nremove checking " + matrixArray.size());
-        for (int i = 0; i < matrixArray.size(); i++) {  // test 1 should stay
+        for (int i = 49998; i < 50002; i++) {
             System.out.println(i + " " + matrixArray.get(i));
         }
         System.out.println("head " + matrixArray.get(0));
         System.out.println("tail " + matrixArray.get(matrixArray.size() - 1));
     }
 
-    private void testAddArray(IArray data, int total) {
+    private void testAddArray(IArray<String> data, int total) {
         long start = System.currentTimeMillis();
 
         for (int j = 0; j < total; j ++)
             data.add(j + ". " + new Date());
 
         System.out.println(data + " testAddArray: " +
-                (System.currentTimeMillis() - start));
+                (System.currentTimeMillis() - start) + "ms");
     }
 
 
-    private void testAddPosArray(IArray data, int position, Object insert) {
+    private void testAddPosArray(IArray<String> data, int position, String insert) {
         long start = System.currentTimeMillis();
 
         data.add(insert, position);
-        System.out.println(data + " testAddArray: " +
-                (System.currentTimeMillis() - start));
+        System.out.println(data + " testAddPosArray: " +
+                (System.currentTimeMillis() - start) + "ms");
     }
 
-    private void testDelPosArray(IArray data, int position) {
+    private void testDelPosArray(IArray<String> data, int position) {
         long start = System.currentTimeMillis();
 
         Object removed = data.remove(position);
-        System.out.println(data + " testAddArray: " +
-                (System.currentTimeMillis() - start) + "; removed: " + removed);
+        System.out.println(data + " testDelPosArray: " +
+                (System.currentTimeMillis() - start) + " ms; removed: " + removed);
     }
-
-
 }
