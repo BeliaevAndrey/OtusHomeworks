5aa19996850b7952d80a839aa85b67d466557e32
Andrei Beliaev <abbot190414@gmail.com>
2023-08-30 18:28:44 +0300

    OTUS homework 006: InsertionSort.java -- shiftBinSearchSort added;

--- HomeWork006/src/app.java
+++ HomeWork006/src/app.java
@@ -9,17 +9,29 @@ public class app {
 
         BubbleSort bubble = new BubbleSort();
         InsertionSort insert = new InsertionSort();
-        int[] arr1 = ArrayGen.setRandom(40);
-        int[] arr2 = ArrayGen.setRandom(40);
+        int[] arr1 = ArrayGen.setRandom(100_00);
+        int[] arr2 = ArrayGen.setRandom(100_00);
         bubble.init(arr1);
         insert.init(arr2);
 
-        System.out.println(Arrays.toString(arr1));
+//        System.out.println(Arrays.toString(arr1));
         bubble.sort();
-        System.out.println(Arrays.toString(arr1));
+        System.out.println(isValid(arr2));
+//        System.out.println(Arrays.toString(arr1));
 
-        System.out.println(Arrays.toString(arr2));
-        insert.shiftSort();
-        System.out.println(Arrays.toString(arr2));
+//        System.out.println(Arrays.toString(arr2));
+        insert.shiftBinSearchSort();
+        System.out.println(isValid(arr2));
+//        System.out.println(Arrays.toString(arr2));
+    }
+
+    static boolean isValid(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i - 1] > arr[i]) {
+                System.out.println("i-1: " + (i - 1) + " i: " + i + " arr[i-1] " + arr[i - 1] + " arr[i] " + arr[i]);
+                return false;
+            }
+        }
+        return true;
     }
 }
