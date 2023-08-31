6172d4a25ea6685d502ca0f07f25e00b65a71977
Andrei Beliaev <abbot190414@gmail.com>
2023-08-31 11:42:38 +0300

    OTUS homework 006: ShellSort.java Ciura, Knuth, Sedgewick optimizations are added

--- HomeWork006/src/app.java
+++ HomeWork006/src/app.java
@@ -3,26 +3,51 @@ import sortings.InsertionSort;
 import sortings.ShellSort;
 import util.ArrayGen;
 
+import java.util.Arrays;
+
 public class app {
     public static void main(String[] args) {
-
+        int N = 1000_000;
         BubbleSort bubble = new BubbleSort();
         InsertionSort insert = new InsertionSort();
         ShellSort shell = new ShellSort();
-        int[] arr1 = ArrayGen.setRandom(100_00, true);
-        int[] arr2 = ArrayGen.setRandom(100_00, true);
+        ShellSort shell2 = new ShellSort();
+        int[] arr1 = ArrayGen.setRandom(N, true);
+        int[] arr2 = ArrayGen.setRandom(N, true);
+        int[] arr3 = ArrayGen.setRandom(N, true);
+        int[] arr4 = ArrayGen.setRandom(N, true);
+
         shell.init(arr1);
-        insert.init(arr2);
-
-//        System.out.println(Arrays.toString(arr1));
-        shell.sort();
+        long start = System.nanoTime();
+        shell.sortCiura();
+        double shellCiuraTime = (System.nanoTime() - start) / 1e9;
         System.out.println(isAscending(arr1));
-//        System.out.println(Arrays.toString(arr1));
 
-//        System.out.println(Arrays.toString(arr2));
-        insert.shiftBinSearchSort();
+        shell.init(arr3);
+        start = System.nanoTime();
+        shell.sortKnuth(N);
+        double shellKnuthTime = (System.nanoTime() - start) / 1e9;
+        System.out.println(isAscending(arr3));
+
+        shell.init(arr4);
+        start = System.nanoTime();
+        shell.sortSedgewick86(N);
+        double shellSedgewickTime = (System.nanoTime() - start) / 1e9;
+        System.out.println(isAscending(arr4));
+
+        shell2.init(arr2);
+        start = System.nanoTime();
+        shell2.sort();
+        double shellSimpleTime = (System.nanoTime() - start) / 1e9;
         System.out.println(isAscending(arr2));
-//        System.out.println(Arrays.toString(arr2));
+
+        System.out.printf(
+                "shellSimpleTime:    %f s" +
+                        "\nshellCiuraTime:     %f s" +
+                        "\nshellKnuthTime:     %f s" +
+                        "\nshellSedgewickTime: %f s\n",
+                shellSimpleTime, shellCiuraTime,
+                shellKnuthTime, shellSedgewickTime);
     }
 
     static boolean isAscending(int[] arr) {
@@ -30,6 +55,7 @@ public class app {
             if (arr[i - 1] > arr[i]) {
                 System.out.println("i-1: " + (i - 1) + " i: " + i + " arr[i-1] " + arr[i - 1] + " arr[i] " + arr[i]);
                 return false;
+
             }
         }
         return true;
--- HomeWork006/src/sortings/ShellSort.java
+++ HomeWork006/src/sortings/ShellSort.java
@@ -18,9 +18,49 @@ public class ShellSort {
     public void sort() {
         for (int gap = length / 2; gap > 0; gap /= 2)
             for (int j = gap; j < length; j++)
-                for (int i = j; i >= gap && array[i - gap] > array[i] ; i -= gap)
+                for (int i = j; i >= gap && array[i - gap] > array[i]; i -= gap)
                     swap(i - gap, i);
     }
+
+    public void sortGaps(int[] gaps) {
+        for (int gap : gaps)
+            for (int j = gap; j < length; j++)
+                for (int i = j; i >= gap && array[i - gap] > array[i]; i -= gap)
+                    swap(i - gap, i);
+    }
+    public void sortCiura() {
+        int[] gapsCiura = {701, 301, 132, 57, 23, 10, 4, 1};   // Ciura gap sequence
+        sortGaps(gapsCiura);
+    }
+    public void sortKnuth(int N) {     // Knuth gap sequence
+        int len = (int) Math.ceil(Math.log(N) / Math.log(3));
+        int[] gaps = new int[len];
+        int limit = N / 3;
+        int gap = 0;
+        for (int k = 1; gap < limit; k++) {
+            gap = (int)((Math.pow(3, k) - 1) / 2);
+            gaps[len - k] = gap;
+        }
+        sortGaps(gaps);
+    }
+
+    public void sortSedgewick86(int N) {    // Sedgewick gap sequence (1986)
+        int len = (int) Math.ceil(Math.log10(N) / Math.log10(2));
+        int[] gaps = new int[len];
+        double sqrtOfTwo = Math.pow(2, 0.5);
+        int gap = 0;
+        for (int k = 0; gap < N / 2; k += 2) {
+            gap = (int) Math.ceil(9 * (Math.pow(2, k) - Math.pow(sqrtOfTwo, k)) + 1);
+            gaps[len - k - 1] = gap;
+        }
+        gap = 0;
+        for (int k = 1; gap < N / 2; k += 2) {
+            gap = (int) Math.ceil(8 * Math.pow(2, k) - 6 * Math.pow(sqrtOfTwo, (k + 1))) + 1;
+            gaps[len - k - 1] = gap;
+        }
+        sortGaps(gaps);
+    }
+
 
 
     void swap(int left, int right) {
