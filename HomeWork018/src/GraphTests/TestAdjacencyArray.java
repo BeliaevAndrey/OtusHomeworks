480e92058fcee885216dd00cc1e27c6abdcc8934
BeliaevAndrey <ab5030107021@yandex.ru>
2024-10-20 07:27:14 +0300

    Homework 18: GraphAdjacencyArray.java added.

---
+++ HomeWork018/src/GraphTests/TestAdjacencyArray.java
@@ -0,0 +1,23 @@
+package GraphTests;
+
+import presents.AdjcencyArray.GraphAdjacencyArray;
+
+public class TestAdjacencyArray {
+
+    public static void main(String[] args) {
+        int[][] adjMatrix = {
+                {0, 1, 0, 0, 0, 0, 0},
+                {1, 0, 1, 0, 0, 0, 1},
+                {0, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 1, 0, 1, 0},
+                {0, 0, 0, 0, 1, 0, 0},
+                {0, 1, 0, 0, 0, 0, 0}
+        };
+
+        GraphAdjacencyArray gaArr = new GraphAdjacencyArray();
+        gaArr.buildFromAdjacencyMatrix(adjMatrix);
+        gaArr.printAArray();
+
+    }
+}
---
+++ HomeWork018/src/presents/AdjcencyArray/GraphAdjacencyArray.java
@@ -0,0 +1,38 @@
+package presents.AdjcencyArray;
+
+public class GraphAdjacencyArray {
+
+    int[][] adjArray;
+
+    int vertAmt;
+
+    public void buildFromAdjacencyMatrix(int[][] aMatrix) {
+        vertAmt = aMatrix.length;
+        adjArray = new int[vertAmt][];
+        for (int i = 0; i < vertAmt; i++) {
+
+            int width = 0, k = 0;
+
+            for (int j = 0; j < vertAmt; j++) width += (aMatrix[i][j] == 1 ? 1 : 0);
+            adjArray[i] = new int[width];
+
+            for (int j = 0; j < vertAmt; j++)
+                if (aMatrix[j][i] == 1) adjArray[i][k++] = (j + 1);
+        }
+
+    }
+
+    public int[][] getAdjArray() { return adjArray; }
+
+    public void printAArray() {
+        for (int i = 0; i < vertAmt; i++) {
+            System.out.printf("%-3d|", i + 1);
+            for (int j = 0; j < adjArray[i].length; j++) {
+                System.out.printf("%3d", adjArray[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+
+}
