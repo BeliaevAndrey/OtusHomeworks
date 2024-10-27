c906119e4c37cc64b589fde2c329cc8e8a79f64f
BeliaevAndrey <ab5030107021@yandex.ru>
2024-10-27 17:07:29 +0300

    Homework 20: CustomLinkedList method sort() refactored;

--- HomeWork020/src/structures/CustomLinkedList.java
+++ HomeWork020/src/structures/CustomLinkedList.java
@@ -58,20 +58,48 @@ public class CustomLinkedList<T extends Comparable<T>> {
         return out;
     }
 
-    public int size() {
-        return this.size;
+    private void insert(int index, Node<T> nodeIn) {
+        if (index < 0 || index > size)
+            throw new IllegalArgumentException("Index is out of bounds for size: " + size);
+        if (index == 0) {
+            nodeIn.next = head;
+            head = nodeIn;
+            size++;
+            return;
+        }
+        Node<T> node = search(index - 1);
+        nodeIn.next = node.next;
+        node.next = nodeIn;
+        size++;
     }
+
+    private Node<T> pop(int index) {
+        if (size == 0) throw new IllegalStateException("Pop from empty list");
+        Node<T> out = head;
+        if (index == 0) {
+            head = head.next;
+            size--;
+            if (head == null) head = new Node<>(null);
+            return out;
+        }
+        Node<T> node = search(index - 1);
+        out = node.next;
+        node.next = node.next.next;
+        size--;
+        return out;
+    }
+
+    public int size() { return this.size; }
 
     public void clear() {
         this.head = new Node<>(null);
         this.size = 0;
     }
 
-    boolean isEmpty() {
-        return size() == 0;
-    }
+    public boolean isEmpty() { return size() == 0; }
 
     public void reverse() {
+        if (size == 0) throw new IllegalStateException("Empty list exception");
         CustomLinkedList<T> tmp = new CustomLinkedList<>();
         int index = size - 1;
         Node<T> current = head;
@@ -82,17 +110,52 @@ public class CustomLinkedList<T extends Comparable<T>> {
         this.head = tmp.head;
     }
 
-    public void sort() {
+    // selection sort (unstable)
+    public void selectionSort() {
         for (int i = 0; i < size - 1; i++) {
             int pos = i;
-            for (int j = i; j < size; j++) {
+            for (int j = i + 1; j < size; j++) {
                 if (search(j).compareTo(search(pos)) < 0) pos = j;
             }
             if (pos != i) swapData(i, pos);
         }
     }
 
-    void swapData(int l, int r) {
+    void swapNodes(int l, int r) {
+        insert(l, pop(r));
+        insert(r, pop(l + 1));
+
+    }
+
+    // stable
+    public void sort() {
+        int lim =  size * (size - 1) / 2;
+        for (int i = 0; i < lim; i++) {
+            Node<T> node = head;
+            int l = 0;
+            while (node.next != null) {
+                if (node.compareTo(node.next) > 0) {
+                    swapNodes(l, l + 1);
+                    break;
+                }
+                node = node.next;
+                l++;
+            }
+            if (i % 10 == 0 && isAscending()) { return; }
+        }
+    }
+
+    // check if list is sorted in ascending order
+    public boolean isAscending() {
+        Node<T> node = head;
+        while (node.next != null) {
+            if (node.compareTo(node.next) > 0) return false;
+            node = node.next;
+        }
+        return true;
+    }
+
+    private void swapData(int l, int r) {
         T tmp = get(l);
         search(l).data = search(r).data;
         search(r).data = tmp;
